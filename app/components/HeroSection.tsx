7040f2d6e5ee6358b469fe9939a9ae14c658cd81
MichaelVertin <77863931+MichaelVertin@users.noreply.github.com>
2025-09-07 07:55:50 -0600

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
 					<Image src="/portrait.jpg" alt="Profile Picture" width={120} height={120} className="rounded-full mx-auto border-4 border-gray-50 shadow-lg" />
 				</div>
 				<h1 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-3 sm:mb-4">Michael Vertin</h1>
-				<p className="text-lg sm:text-xl text-gray-600 mb-6 sm:mb-8">Sotftware Developer</p>
+				<p className="text-lg sm:text-xl text-gray-600 mb-6 sm:mb-8">Sotftware Engineer</p>
 				<p className="text-gray-600 max-w-2xl mx-auto text-sm sm:text-base px-4">
 					I’m a software engineer with experience in computer science and mathematics. My focus is on programming clean, reliable software that connects abstract theory with practical applications. I enjoy applying concepts like graph theory, set theory, and logical reasoning to build efficient systems and accurate representations of real-world processes.
 				</p>
